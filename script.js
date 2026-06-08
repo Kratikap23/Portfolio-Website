8274efc6db800ae76325e154ac6fd3a7f8f16ef7
Kratikap23 <kratikap665@gmail.com>
2026-06-08 13:49:32 +0530

    Apply Dark Mode

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function toggleDescription(btn) {
         btn.textContent = "Show Less";
     }
 }
+function darkModeBtn(btn) {
+
+    document.body.classList.toggle("dark-mode");
+    const icon = document.querySelector("#darkModeBtn i");
+
+    if (document.body.classList.contains("dark-mode")) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+}
